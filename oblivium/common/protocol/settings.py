8774d472ca1ccc61eaacc4c695c457d6fcc3ceaa
sinistro14 <tiago14_ribeiro@hotmail.com>
2018-04-24 10:40:14 +0100

    Add Protocol related settings

---
+++ oblivium/common/protocol/settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.6
+
+#  1-2 Oblivious Transfer Protocol
+DATA_SET_SIZE_VALUE = 2
+
+
+# Data set
+
+# Data set file path, which will be used by the server
+DATA_SET_FILE_PATH = "oblivium/server/resources/data_set.txt"
+DATA_SET_FILE_ACCESS = "r"

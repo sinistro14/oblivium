7d522df6f49fc551ca7a4425e70bae0e4838230d
sinistro14 <tiago14_ribeiro@hotmail.com>
2018-04-24 10:32:14 +0100

    Add RequestMessage
    Where the client sends v, to the servet

--- oblivium/common/messages/request_message.py
+++ oblivium/common/messages/request_message.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3.6
 
 
-class InitialMessage:
+class RequestMessage:
+    """
+    Message sent by the client to the server, v = (x'b + k^e) mod N
+    """
+    __v = None
 
-    __public_key = None
-    __number_of_messages = 2
-    __random_messages = []
+    def __init__(self, v):
+        self.__v = v
 
-    def __init__(self, public_key, number_of_messages, random_messages):
-        self.__public_key = public_key
-        self.__number_of_messages = number_of_messages
-        self.__random_messages = random_messages
-
-    def get_public_key(self):
-        return self.__public_key
-
-    def get_number_of_messages(self):
-        return self.__number_of_messages
-
-    def get_random_messages(self):
-        return self.__random_messages
+    def get_v(self):
+        return self.__v

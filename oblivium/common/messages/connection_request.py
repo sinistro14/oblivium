7ea0e73aadd49bd479d9f5d42fbfaf6c2ae9e66e
sinistro14 <tiago14_ribeiro@hotmail.com>
2018-04-24 10:33:13 +0100

    Add ConnectionRequest
    Message where the client requests the connection to the server

---
+++ oblivium/common/messages/connection_request.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+
+
+class ConnectionRequest:
+    """Represents the presentation message of a given client"""
+
+    __string = "ping"
+
+    def get_string(self):
+        return self.__string

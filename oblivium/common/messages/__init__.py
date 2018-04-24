13389613378dfcfef86935280d00cc7291b788bf
sinistro14 <tiago14_ribeiro@hotmail.com>
2018-04-24 10:35:36 +0100

    Add ResponseMessage
    Server sends the available data sets topics to the client, as well as his public key and set of random messages

--- oblivium/common/messages/__init__.py
+++ oblivium/common/messages/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.6
 
-from oblivium.common.messages.initial_message import InitialMessage
+from oblivium.common.messages.connection_request import ConnectionRequest
+from oblivium.common.messages.request_message import RequestMessage
 from oblivium.common.messages.response_message import ResponseMessage
--- oblivium/common/messages/response_message.py
+++ oblivium/common/messages/response_message.py
@@ -2,11 +2,30 @@
 
 
 class ResponseMessage:
+    """
+    Represents the initial Server message, where the available data
+    is presented to the client, as well as the server's public key
+    and the set of random messages to be used, as required by the
+    protocol
+    """
 
-    __string = ""
+    __public_key = None
+    __topics = ""
+    __random_messages = []
 
-    def __init__(self, string):
-        self.__string = string
+    def __init__(self, public_key, topics, random_messages):
+        self.__public_key = public_key
+        self.__topics = topics
+        self.__random_messages = random_messages
 
-    def get_string(self):
-        return self.__string
+    def get_public_key(self):
+        return self.__public_key
+
+    def get_topics(self):
+        return self.__topics
+
+    def get_number_of_topics(self):
+        return len(self.__topics)
+
+    def get_random_messages(self):
+        return self.__random_messages

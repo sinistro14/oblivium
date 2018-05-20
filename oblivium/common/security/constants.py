37b0931841d4e6c6c35ff9b688677698f14c5c72
sinistro14 <tiago14_ribeiro@hotmail.com>
2018-05-20 11:25:54 +0100

    Fixed padding issue

--- oblivium/common/security/constants.py
+++ oblivium/common/security/constants.py
@@ -5,9 +5,9 @@ BYTE_ORDER = "big"
 DEFAULT_ENCODING = "utf-8"
 
 # RSA Protocol Specs
-RSA_KEY_SIZE = 1024              # Key size
-RSA_PUBLIC_EXPONENT = 65537      # Public exponent (e)
+RSA_KEY_SIZE = 1024                 # Key size
+RSA_PUBLIC_EXPONENT = 65537         # Public exponent (e)
 
 # Random message generation Specs
-NUMBER_OF_RANDOM_BYTES = 128     # Number of random bytes generated for (xi) random messages
-MAX_GENERATION_LIMIT = 2 ** 200  # Limit value to be used for number generation
+NUMBER_OF_RANDOM_BYTES = 128        # Number of random bytes generated for (xi) random messages
+MAX_GENERATION_LIMIT = pow(2, 2000)  # Limit value to be used for number generation

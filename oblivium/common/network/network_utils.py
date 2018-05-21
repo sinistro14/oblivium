39d3ae34b8d8a278f76aabf935026066cf81af5c
sinistro14 <tiago14_ribeiro@hotmail.com>
2018-05-21 02:20:49 +0100

    Delete unused classes

--- oblivium/common/network/network_utils.py
+++ oblivium/common/network/network_utils.py
@@ -3,18 +3,6 @@
 import base64
 import pickle
 
-from oblivium.common.network import constants
-
-
-#  TODO not in use, delete later
-def string_to_base64(string):
-    return base64.b64encode(string.encode(constants.DEFAULT_ENCODING))
-
-
-#  TODO not in use, delete later
-def base64_to_string(byte):
-    return base64.b64decode(byte).decode(constants.DEFAULT_ENCODING)
-
 
 def object_to_base64(obj):
     return base64.b64encode(pickle.dumps(obj))
